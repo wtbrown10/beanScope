eb067bff1f669ee5fcf0c504d9e7094525791523
Will brown <wtbrown10@gmail.com>
2021-09-27 22:24:39 -0400

    sout

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -11,12 +11,14 @@ public class Application {
             Car car1 = ctx.getBean(Car.class);
             Car car2 = ctx.getBean(Car.class);
             Car car3 = ctx.getBean(Car.class);
+            Car car4 = ctx.getBean(Car.class);
 
             System.out.println("Car 1: " + car1);
             System.out.println("Car 2: " + car2);
+            System.out.println("Car 3: " + car3);
+            System.out.println("Car 4: " + car4);
 
-
-            ((AnnotationConfigApplicationContext)ctx).close();
+        ((AnnotationConfigApplicationContext)ctx).close();
 
     }
 }
